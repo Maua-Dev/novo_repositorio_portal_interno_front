52821bfe7d8fc0020bb26a26684fada6a35690c7
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-05-18 21:51:29 -0300

    feat: implementa primeira versao do componente navbar e adiciona dependencia do react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
+import { FaPlusSquare, FaUser } from "react-icons/fa";
+import { FaClockRotateLeft } from "react-icons/fa6";
+import { BsMoonStars } from "react-icons/bs";
+import { IoMdExit, IoIosArrowForward } from "react-icons/io";
 
 function App() {
 
+  const buttonClasses = "p-2 text-2xl cursor-pointer hover:md:text-blue-500 w-fit text-gray-800 transition-all"
+
   return (
-    <div className='flex w-full h-screen justify-center items-center'>
-      <h1 className='flex bg-blue-700 w-200 h-30 justify-center items-center text-white font-bold rounded-4xl border-10 border-blue-400'>Desfazendo projeto vite básico e adicionando tailwind</h1>
+    <div className="w-full h-screen flex">
+      <nav className="w-full bg-gray-200 flex justify-center p-2 gap-5 place-self-end md:w-28 md:p-0 md:h-screen md:flex-col md:justify-normal">
+        <div className="flex md:flex-col gap-4 place-items-center">
+          <img src="/src/assets/logo_dev.png" alt="Logo DEV" className="hidden md:block max-w-1/2 place-self-center mt-10" />
+          <button className={buttonClasses}><FaUser /></button>
+          <button className={buttonClasses}><FaPlusSquare /></button>
+          <button className={buttonClasses}><FaClockRotateLeft /></button>
+        </div>
+        <div className="flex md:flex-col gap-4 place-items-center">
+          <button className={buttonClasses}><BsMoonStars /></button>
+          <button className={buttonClasses}><IoIosArrowForward /></button>
+          <button className={buttonClasses}><IoMdExit /></button>
+        </div>
+      </nav>
     </div>
   )
 }
