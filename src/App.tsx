2a5df8e5fc7f70b4d872653464e40ab1e9be8d7c
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-05-19 19:22:43 -0300

    feat: adiciona funcionalidade de clique nos botoes da navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,35 @@ import { FaPlusSquare, FaUser } from "react-icons/fa";
 import { FaClockRotateLeft } from "react-icons/fa6";
 import { BsMoonStars } from "react-icons/bs";
 import { IoMdExit, IoIosArrowForward } from "react-icons/io";
+import { useState } from 'react';
 
 function App() {
 
-  const buttonClasses = "p-2 text-2xl cursor-pointer hover:lg:text-blue-500 w-fit text-gray-800 transition-all"
+  const buttonClasses = "p-2 text-2xl cursor-pointer hover:lg:text-blue-500 w-fit text-gray-800 transition-all flex items-center"
+  const labelClasses = "hidden md:ps-7 md:block text-lg font-medium"
+  const [opened, setOpened] = useState(false)
 
   return (
     <div className="w-full h-screen flex">
-      <nav className="w-full flex justify-center p-2 place-self-end md:w-28 md:p-0 md:h-screen md:flex-col md:justify-between transition-all shadow">
-        <div className="md:hidden flex w-full justify-center absolute top-0 shadow">
+      <nav className={`w-full flex justify-center p-2 place-self-end transition-all shadow md:p-0 md:h-screen md:flex-col md:justify-between fixed ${opened ? "md:w-52" : "md:w-32"}`}>
+        <div className="md:hidden flex w-full justify-center top-0 shadow fixed">
           <img src="/src/assets/logo_dev.png" alt="Logo DEV" className="max-w-12 place-self-center m-3" />
         </div>
-        <div className="flex md:flex-col gap-4 place-items-center">
+        <div className={`flex md:flex-col`}>
           <img src="/src/assets/logo_dev.png" alt="Logo DEV" className="hidden md:block max-w-16 place-self-center my-10" />
-          <button className={buttonClasses}><FaUser /></button>
-          <button className={buttonClasses}><FaPlusSquare /></button>
-          <button className={buttonClasses}><FaClockRotateLeft /></button>
+          <div className={`flex md:flex-col gap-4 transition-all ${opened ? "place-items-start ps-10" : "place-items-center"}`}>
+            <button className={buttonClasses}><FaUser />{opened && <span className={labelClasses}>Perfil</span>}</button>
+            <button className={buttonClasses}><FaPlusSquare />{opened && <span className={labelClasses}>Tarefa</span>}</button>
+            <button className={buttonClasses}><FaClockRotateLeft />{opened && <span className={labelClasses}>Histórico</span>}</button>
+          </div>
         </div>
         <div className="flex md:flex-col gap-4 place-items-center ps-4 md:ps-0 md:mb-15">
           <button className={buttonClasses}><BsMoonStars /></button>
-          <button className={buttonClasses + " hidden md:flex"}><IoIosArrowForward /></button>
+          <button className={buttonClasses + ` hidden md:flex ${opened ? "rotate-180" : "rotate-0"}`} onClick={() => setOpened(!opened)}><IoIosArrowForward /></button>
           <button className={buttonClasses}><IoMdExit /></button>
         </div>
       </nav>
-    </div>
+    </div >
   )
 }
 
